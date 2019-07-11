2980edc70d40e98db228c320724cb11cea4ab59c
dps0610 <dps0610@hotmail.com>
2019-07-11 18:21:58 -0400

    created html, js, css files. base html page, and movies array.

---
+++ javascript.js
@@ -0,0 +1,2 @@
+var movies = ["Clue", "Blade Runner 2049", "Gremlins", "Gremlins 2", "Drive", "Logan", "Mad Max Fury Road", "My Cousin Vinny", "Detective Pikachu", "There Will be Blood", "Tombstone"];
+
